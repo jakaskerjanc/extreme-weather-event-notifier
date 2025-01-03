6f97ab96f2c2c6080a7852b91c4cc88fc6616ef1
jakaskerjanc <100130505+jakaskerjanc@users.noreply.github.com>
2025-01-03 23:49:21 +0100

    Fix notification register status diplay

--- web/pages/notificationRegister.vue
+++ web/pages/notificationRegister.vue
@@ -11,7 +11,7 @@
         <v-card-title> Notification Status </v-card-title>
         <v-card-text>
           <p> Client ID: {{ registerStatus.clientId }} </p>
-          <p> Notifications enabled: {{ registerStatus.registered }} </p>
+          <p> Notifications: {{ registerStatus.isRegistered }} </p>
         </v-card-text>
       </v-card>
     </div>
