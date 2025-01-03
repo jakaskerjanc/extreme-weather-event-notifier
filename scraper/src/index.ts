43d40c995ff02ac9e4976dc07c435c63b6f5307b
jakaskerjanc <100130505+jakaskerjanc@users.noreply.github.com>
2025-01-03 20:26:41 +0100

    Add health check to scraper

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,7 +1,9 @@
 import { runArsoScrapper } from './scraper'
 import type { ArsoSevereEvent } from './scraper'
-
 import { openChannel, sendMessage } from './rabbitmq'
+import express from 'express'
+
+const isReady = false
 
 async function main() {
   const { connection, channel } = await openChannel()
@@ -9,6 +11,18 @@ async function main() {
   runArsoScrapper(async (events: ArsoSevereEvent[]) => {
     await sendMessage(channel, 'new_weather_events', events)
   })
+
+  const app = express()
+
+  app.get('/healthz', (_req, res) => {
+    if (!isReady) {
+      res.status(500).send('Not ready')
+    } else {
+      res.status(200).send('I am alive!')
+    }
+  })
+
+  app.listen(3001)
 
   process.on('SIGTERM', () => {
     connection.close()
